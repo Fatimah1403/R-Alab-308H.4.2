011e3fd2d34b6c0290226331317ffc589e1b42cf
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-10-21 13:37:10 -0400

    Get Even answered

--- lab-answer.js
+++ lab-answer.js
@@ -1,6 +1,11 @@
 let log = console.log;
-// Easy Going
-let i = 0;
-for (i = 1; i <= 20; i++) {
-    log(i);
+// let log = console.log;
+// // Easy Going
+// let i = 0;
+// for (i = 1; i <= 20; i++) {
+//     log(i);
+// }
+// Get Even
+for (let y = 0; y <= 200; y +=2) {
+    log(y)
 }
