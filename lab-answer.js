bf1cfe60d062807252371811c94e1265e21a1c98
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-10-21 19:19:33 -0400

    Fizz Buzz answered

--- lab-answer.js
+++ lab-answer.js
@@ -1,11 +1,24 @@
 let log = console.log;
-// let log = console.log;
-// // Easy Going
-// let i = 0;
-// for (i = 1; i <= 20; i++) {
-//     log(i);
+// // let log = console.log;
+// // // Easy Going
+// // let i = 0;
+// // for (i = 1; i <= 20; i++) {
+// //     log(i);
+// // }
+// // Get Even
+// for (let y = 0; y <= 200; y +=2) {
+//     log(y)
 // }
-// Get Even
-for (let y = 0; y <= 200; y +=2) {
-    log(y)
+
+// FIZZ BUZZ
+for (let z = 1; z <= 100; z++) {
+    if (z % 3 === 0 && z % 5 === 0) {
+        log("FizzBuzz");
+    } else if (z % 3 === 0) {
+        log("Fizz");
+    } else if (z % 5 === 0) {
+        log("Buzz");
+    } else {
+        log(z);
+    }
 }
